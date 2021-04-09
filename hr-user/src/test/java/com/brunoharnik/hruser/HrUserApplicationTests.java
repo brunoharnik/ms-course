cfc025f5de2312734bde11e2c20d22413d7c609a
brunoharnik <brunoharnik@gmail.com>
2021-04-08 22:20:27 -0300

    hr user projeto

---
+++ hr-user/src/test/java/com/brunoharnik/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoharnik.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
